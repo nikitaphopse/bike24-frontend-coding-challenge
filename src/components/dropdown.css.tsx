b1237434c6b8841005cef811a0efb42fca84951a
Nikita Phopse <nikitaphopse@gmail.com>
2023-01-26 15:32:34 +0100

    Added slider and css

--- src/components/dropdown.css.tsx
+++ src/components/dropdown.css.tsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
+export const MainContainer = styled.div`
+  width: 100%;
+  padding: 10px;
+`;
+
 export const DropdownBox = styled.select`
   width: 450px;
   padding: 10px 15px;
@@ -9,4 +14,30 @@ export const DropdownBox = styled.select`
   border-radius: 6px;
   outline: 0;
   color: #4c4e52;
+`;
+
+export const SliderContainer = styled.div`
+  display: inline-block;
+  width: 30%;
+  margin: 0px 15px;
+  margin: 0 6%;
+`;
+
+export const AmountContainer = styled.div`
+  display: inline-block;
+  margin: 0 10px;
+`;
+
+export const Count = styled.span`
+  padding: 10px;
+  border: 2px solid #abb0b8;
+  margin-right: 10px;
+  color: #4c4e52;
+  border-radius: 6px;
+`;
+
+export const ErrorMsg = styled.div`
+  color: red;
+  font-size: 12px;
+  margin: 5px 0;
 `;
--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -1,11 +1,23 @@
 import data from "./../products.json";
 import { useState, useEffect } from "react";
-import { DropdownBox } from "./dropdown.css";
+import {
+  MainContainer,
+  DropdownBox,
+  SliderContainer,
+  AmountContainer,
+  Count,
+  ErrorMsg,
+} from "./dropdown.css";
+import Slider from "rc-slider";
+import { Button } from "react-bootstrap";
+import "../../src/style.css";
 
 const Dropdown = ({ cartArr }: any) => {
   const initialProduct = { id: "" };
   const [selProduct, setProduct] = useState<any>(initialProduct);
   const [currCount, setCount] = useState<any>(1);
+  const [maxCount, setMaxCount] = useState<any>();
+  const [btnDisabled, updateButton] = useState<any>();
 
   useEffect(() => {
     if (!cartArr.length) {
@@ -14,6 +26,17 @@ const Dropdown = ({ cartArr }: any) => {
     }
   }, [cartArr]);
 
+  useEffect(() => {
+    setMaxCount(Math.max(...data.map((o) => o.maxAmount)));
+  }, []);
+
+  useEffect(() => {
+    const btnState = !!(
+      document.getElementsByClassName("error").length > 0 || !selProduct.id
+    );
+    updateButton(btnState);
+  }, [selProduct.id, currCount, cartArr]);
+
   const handleProductChange = (event: any) => {
     const updatedProduct = data.find(
       (product: any) => product.id === event.target.value
@@ -21,20 +44,67 @@ const Dropdown = ({ cartArr }: any) => {
     setProduct(updatedProduct);
   };
 
+  const updateCount = (count: any) => {
+    setCount(count);
+  };
+
+  const isProductSelected = () => {
+    return cartArr.findIndex(({ id }: any) => id === selProduct?.id) !== -1;
+  };
+
   return (
-    <div>
+    <MainContainer>
       <DropdownBox onChange={handleProductChange} value={selProduct.id}>
         <option disabled value="">
           Select a Product
         </option>
         {data.map((product) => (
-          <option
-            key={product.id}
-            value={product.id}
-          >{`${product.productName} - $${product.price}`}</option>
+          <option key={product.id} value={product.id}>{`${
+            product.productName
+          } - $${product.price.toFixed(2)}`}</option>
         ))}
       </DropdownBox>
-    </div>
+      <br />
+      {selProduct.id && (
+        <>
+          <SliderContainer>
+            <Slider
+              onChange={updateCount}
+              min={1}
+              max={maxCount}
+              defaultValue={currCount}
+              step={1}
+            />
+          </SliderContainer>
+          <AmountContainer>
+            <Count>{currCount}</Count>
+            <span className="margin-right-10">X</span>{" "}
+            <span className="margin-right-10">{selProduct.price}</span>
+            <span className="margin-right-10">=</span>{" "}
+            <span className="margin-right-10">
+              {(currCount * selProduct.price).toFixed(2)}
+            </span>
+          </AmountContainer>
+        </>
+      )}
+
+      <Button disabled={btnDisabled}>Add to Cart</Button>
+      {currCount > selProduct.maxAmount &&
+        cartArr.length !== 10 &&
+        !isProductSelected() && (
+          <ErrorMsg className="error">
+            *Quantity limit for product exceeded
+          </ErrorMsg>
+        )}
+      {cartArr.length === 10 && (
+        <ErrorMsg className="error">*Maximum product limit reached</ErrorMsg>
+      )}
+      {isProductSelected() && cartArr.length !== 10 && (
+        <ErrorMsg className="error">
+          *Product is already present in the cart
+        </ErrorMsg>
+      )}
+    </MainContainer>
   );
 };
 
